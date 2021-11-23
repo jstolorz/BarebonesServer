183da515d39a338ee5f0f5667bb0ef799b048924
Janusz <31537161+jstolorz@users.noreply.github.com>
2021-11-23 17:44:45 +0100

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <WS2tcpip.h>
+
+#pragma comment (lib,"ws2_32.lib")
+
+using namespace std;
+
+int main() {
+
+    // Initialize winsock
+
+    // create a socket
+
+    // Bind the socket to an ip address amd port
+
+    // Tell winsocket the socket is for listening
+
+    // Wait for a connection
+
+    // Close listening socket
+
+    // While loop: accept and echo message back to client
+
+
+    // Close the socket
+    // Shutdown winsock
+
+    return 0;
+}
